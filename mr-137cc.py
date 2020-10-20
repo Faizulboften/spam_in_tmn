d46dcd0bb958a128fd9eab5b363116917323c13b
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-20 10:40:23 +0800

    Update mr-137cc.py

--- mr-137cc.py
+++ mr-137cc.py
@@ -17,8 +17,12 @@ def baner():
 ▄█ █▀▀ █▀█ █░▀░█  |         | \033[32;1mCreator : Faizul.Boften
                                 |\033[32;1mFacebook : Faizul                                                  
                                 |-----------------------------------|''')
-                             
-def kata(s):
+  
+
+
+
+
+    def kata(s):
     for c in s + "\n":
         sys.stdout.write(c)
         sys.stdout.flush()
